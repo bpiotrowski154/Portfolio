0ffda8a626c24b070444ed2850b12a1db7f35692
bpiotrowski154 <88128424+bpiotrowski154@users.noreply.github.com>
2024-01-09 18:47:37 -0600

    New Card Response
    Changed the cards from the react-card-flip component back to the original css classes. There was no way to fix the react-card-flip from my research. Now the cards will transition to the background image when being hovered over

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,70 +1,74 @@
 import React, {useState} from 'react'
-import MultipleCards from '../MultCards'
 import SOS from '../assets/sosimg.png'
 import Capstone from '../assets/capstoneimg.png'
 import ColorDarkGreen from '../assets/color183d3d.png'
 import ReactCardFlip from 'react-card-flip'
 
+
+const projects = [
+    {
+        imageURL: SOS,
+        backURL: ColorDarkGreen,
+        gitURL: "https://github.com/bpiotrowski154/SOS-Board-Game",
+        demoURL: "/",
+        name: "SOS Game"
+    },
+    {
+        imageURL: Capstone,
+        backURL: ColorDarkGreen,
+        gitURL: "/",
+        demoURL: "/",
+        name: "Capstone Project"
+    },
+];
+
 const Projects = () => {
-    const [flip, setFlip] = useState(false);
+
+    const [flip, setFlip] = useState(false)
 
     const handleClick = () => {
         setFlip(!flip);
     };
-
+    
   return (
-    <div name='projects' className='w-full md:h-screen bg-[#0F0F0F]'>
-        {MultipleCards()}
+    <div name='projects' className='w-full md:h-screen bg-[#0F0F0F]'>        
     
         {/* Container */}
-        {/* <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#5C8374]'>Projects</p>
                 <p className='py-6'>// Check out some of my projects</p>
-            </div> */}
+            </div>
 
             {/* Projects Grid Container */}
-            {/* <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-4' > */}
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-4' >
                 
-                {/* Projects Grid Item */}
-                {/* <ReactCardFlip isFlipped={flip} flipDirection='horizontal'>
-                    <div onClick={handleClick} style={{backgroundImage: `url(${SOS})`}} className='group container rounded-md flex justify-center items-center mx-auto content-div2 hover:cursor-pointer'></div>
-                    <div onClick={handleClick} style={{backgroundImage: `url(${ColorDarkGreen})`}} 
-                         className='container rounded-md flex flex-col justify-center items-center mx-auto hover:cursor-pointer content-div2 text-[#93B1A6]flex'>
-                        <div className='text-2xl font-bold tracking-wider'>
-                            SOS Game
-                        </div>
-                        <div className='pt-8 text-center'>
-                            <a href="/"  className='text-white border-2 hover:bg-[#5C8374] hover:border-[#5C8374] px-4 py-3 m-2 items-center'>
-                                Demo
-                            </a>
-                            <a href="https://github.com/bpiotrowski154/SOS-Board-Game" target='_blank' className='text-white border-2 hover:bg-[#5C8374] hover:border-[#5C8374] px-4 py-3 m-2 items-center'>
-                                GitHub
-                            </a>
-                        </div>
-                    </div>
-                </ReactCardFlip> */}
+                {/* Grid Item */}
+                {projects.map((project, index) => (
+                    <div 
+                        key = {index} 
+                        style={{backgroundImage: `url(${project.imageURL})`}} 
+                        className='group container rounded-md flex justify-center text-center items-center mx-auto content-div'
+                    >
 
-                {/* Projects Grid Item */}
-                {/* <ReactCardFlip isFlipped={flip} flipDirection='horizontal'>
-                    <div onClick={handleClick} style={{backgroundImage: `url(${Capstone})`}} className='group container rounded-md flex justify-center items-center mx-auto content-div2 hover:cursor-pointer'></div>
-                    <div onClick={handleClick} style={{backgroundImage: `url(${ColorDarkGreen})`}} 
-                         className='container rounded-md flex flex-col justify-center items-center mx-auto hover:cursor-pointer content-div2 text-[#93B1A6]flex'>
-                        <div className='text-2xl font-bold tracking-wider'>
-                            Capstone Project
-                        </div>
-                        <div className='pt-8 text-center'>
-                            <a href="/"  className='text-white border-2 hover:bg-[#5C8374] hover:border-[#5C8374] px-4 py-3 m-2 items-center'>
-                                Demo
-                            </a>
-                            <a href="https://github.com/bpiotrowski154/SOS-Board-Game" target='_blank' className='text-white border-2 hover:bg-[#5C8374] hover:border-[#5C8374] px-4 py-3 m-2 items-center'>
-                                GitHub
-                            </a>
+                        {/* Hover Effect for Images */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <div className='text-2xl font-medium tracking-wider'>
+                                {project.name}
+                            </div>
+                            <div className='pt-8 text-center'>
+                                <a href={project.demoURL}  className='text-white border-2 hover:bg-[#5C8374] hover:border-[#5C8374] px-4 py-3 m-2 items-center'>
+                                    Demo
+                                </a>
+                                <a href={project.gitURL} target='_blank' className='text-white border-2 hover:bg-[#5C8374] hover:border-[#5C8374] px-4 py-3 m-2 items-center'>
+                                    GitHub
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </ReactCardFlip> */}
-            {/* </div>
-        </div> */}
+                ))}
+            </div>
+        </div>
     </div>
   )
 }
